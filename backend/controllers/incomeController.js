f02c6df87d3537ae90dc3443233fd59425706896
shashperera <sanduni.p@sliit.lk>
2023-11-05 00:25:59 +0200

    Add Delete method

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,5 +1,15 @@
 const incomeModel = require("../models/incomeModel")
 
+exports.getIncome = async(req, res) => {
+    try {
+        const incomes = await incomeModel.find().sort({createdAt : -1})
+        res.status(200).json(incomes)
+    } catch (error) {
+        res.status(500).json({message: 'Server error'})
+    }
+}
+
+
 exports.addIncome = async(req, res) => {
     // console.log(req.body);
     const {title, amount, category, description, date} = req.body
@@ -27,7 +37,22 @@ exports.addIncome = async(req, res) => {
         res.status(200).json({message: 'Income added'})
         
     } catch (error) {
+        res.status(500).json({message: 'Server error'})
+
         
     }
+
+}
+
+exports.deleteIncome = async(req, res) => {
+    const {id} = req.params;
+    incomeModel.findByIdAndDelete(id)
+        .then((income) => {
+            res.status(200).json({message: 'Income deleted'})
+        })
+        .catch((err) => {
+            res.status(500).json({message: 'Server error for delete'})
+
+        })
 
 }
