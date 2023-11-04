0d48d526a4dc197eebb46f80316c2cb12568903d
shashperera <sanduni.p@sliit.lk>
2023-11-04 19:10:54 +0200

    GET request

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,18 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
+//middleswares
+app.use(express.json())
+app.use(cors()) //Cross-Origin Resource Sharing- acessing server
+
+app.get('/',(req,res)=> {
+    res.send('Hi world') //check response using postman - http://localhost:5000/
+})
+
 const server = () => {
-    console.log('You are listening to PORT : ', PORT)
+    app.listen(PORT, () => {
+        console.log('You are listening to PORT : ', PORT)
+    } )
 }
 
 server()
